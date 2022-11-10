15f408aa2e4bb55521f7d290c2b3ab02198b75f6
Peter van Garderen <117899309+Peter-Programic@users.noreply.github.com>
2022-11-10 09:52:16 +0100

    Added Exceptions in call function depending on response status

--- src/DistanceMatrix.php
+++ src/DistanceMatrix.php
@@ -5,6 +5,14 @@ namespace Programic\DistanceMatrix;
 use Carbon\Carbon;
 use Exception;
 use GuzzleHttp\Client as GuzzleClient;
+use Programic\DistanceMatrix\Exceptions\InvalidKeyException;
+use Programic\DistanceMatrix\Exceptions\InvalidRequestException;
+use Programic\DistanceMatrix\Exceptions\MaxDimensionsExceededException;
+use Programic\DistanceMatrix\Exceptions\MaxElementsExceededException;
+use Programic\DistanceMatrix\Exceptions\OverDailyLimitException;
+use Programic\DistanceMatrix\Exceptions\OverQueryLimitException;
+use Programic\DistanceMatrix\Exceptions\RequestDeniedException;
+use Programic\DistanceMatrix\Exceptions\UnknownErrorException;
 
 class DistanceMatrix
 {
@@ -20,13 +28,28 @@ class DistanceMatrix
     protected $departure_time = 'now';
 
     /**
+     * Distance Matrix statuses
+     *
+     * View this page for more information:
+     * https://developers.google.com/maps/documentation/distance-matrix/distance-matrix#DistanceMatrixStatus
+     **/
+    protected const STATUS_OK = 'OK';
+    protected const STATUS_INVALID_REQUEST = 'INVALID_REQUEST';
+    protected const STATUS_MAX_ELEMENTS_EXCEEDED = 'MAX_ELEMENTS_EXCEEDED';
+    protected const STATUS_MAX_DIMENSIONS_EXCEEDED = 'MAX_DIMENSIONS_EXCEEDED';
+    protected const OVER_DAILY_LIMIT = 'OVER_DAILY_LIMIT';
+    protected const OVER_QUERY_LIMIT = 'OVER_QUERY_LIMIT';
+    protected const STATUS_REQUEST_DENIED = 'REQUEST_DENIED';
+
+    /**
      * DistanceMatrix constructor.
-     * @throws Exception
+     *
+     * @throws InvalidKeyException
      */
     public function __construct()
     {
         if (! config('services.google.distance.key')) {
-            throw new Exception('Google Distance key not set');
+            throw new InvalidKeyException();
         }
 
         return $this;
@@ -73,24 +96,48 @@ class DistanceMatrix
      * Make a call to $url and download its contents with cURL
      * @param string $url
      * @return json_decoded contents of $url
+     *
+     * @throws InvalidRequestException
+     * @throws MaxElementsExceededException
+     * @throws MaxDimensionsExceededException
+     * @throws OverDailyLimitException
+     * @throws OverQueryLimitException
+     * @throws RequestDeniedException
+     * @throws UnknownErrorException
      */
     private function call($url, $params)
     {
-        $client = new GuzzleClient();
-        $request = $client->getAsync($url, [
-            'query' => $params,
-            'verify' => false,
-            'headers' => [
-                'Accept' => 'application/hal+json',
-                'Origin' => '*',
-            ]
-        ]);
+        try {
+            $client = new GuzzleClient();
+            $request = $client->getAsync($url, [
+                'query' => $params,
+                'verify' => false,
+                'headers' => [
+                    'Accept' => 'application/hal+json',
+                    'Origin' => '*',
+                ]
+            ]);
 
-        $response = $request->wait();
+            $response = $request->wait();
 
-        $status = $response->getStatusCode();
-        $body = $response->getBody()->getContents();
+            $body = json_decode($response->getBody()->getContents());
+        } catch (\Exception $exception) {
+            throw new InvalidRequestException();
+        }
 
-        return json_decode($body);
+        $bodyStatus = strtoupper($body->status) ?? self::STATUS_INVALID_REQUEST;
+        if ($bodyStatus === self::STATUS_OK) {
+            return $body;
+        }
+
+        switch ($bodyStatus) {
+            case self::STATUS_INVALID_REQUEST: throw new InvalidRequestException();
+            case self::STATUS_MAX_ELEMENTS_EXCEEDED: throw new MaxElementsExceededException();
+            case self::STATUS_MAX_DIMENSIONS_EXCEEDED: throw new MaxDimensionsExceededException();
+            case self::OVER_DAILY_LIMIT: throw new OverDailyLimitException();
+            case self::OVER_QUERY_LIMIT: throw new OverQueryLimitException();
+            case self::STATUS_REQUEST_DENIED: throw new RequestDeniedException();
+            default: throw new UnknownErrorException();
+        };
     }
 }
